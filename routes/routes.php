0082b54b6f54e43148824bddcff61845f60f0ded
Scott Webster Wood <40869341+scottw-finao@users.noreply.github.com>
2023-05-04 14:35:43 -0400

    tweaking routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,1 +1,13 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Treii28\Grampsdb\Http\Controllers\GrampersController;
+
+Route::middleware('web')
+    //->namespace($this->namespace)
+    ->prefix('gramps')
+    ->group(
+        function () {
+            Route::resource('grampers', GrampersController::class);
+        }
+    );
