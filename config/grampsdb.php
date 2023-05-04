5d202ca458defdf71067646f020961ee6709c861
Scott Webster Wood <40869341+scottw-finao@users.noreply.github.com>
2023-05-04 13:49:03 -0400

    grrr, trying to get package to configure two different database connections and it's not behaving

--- config/grampsdb.php
+++ config/grampsdb.php
@@ -5,17 +5,17 @@ return [
         'default' => env('GRAMPSDB_CONNECTION', 'grampsdb'),
 
         'connections' => [
-            'grampsdb' => [
-                'driver' => 'sqlite',
-                'url' => env('GRAMPSDB_URL'),
-                'database' => database_path(env('GRAMPS_SQLITE', 'data/grampsdb.sqlite')),
-                'prefix' => '',
-                'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            ],
             'woodgen' => [
                 'driver' => 'sqlite',
                 'url' => env('WOODGENDB_URL'),
                 'database' => database_path(env('WOODGEN_SQLITE', 'data/woodgen.sqlite')),
+                'prefix' => '',
+                'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            ],
+            'grampsdb' => [
+                'driver' => 'sqlite',
+                'url' => env('GRAMPSDB_URL'),
+                'database' => database_path(env('GRAMPS_SQLITE', 'data/grampsdb.sqlite')),
                 'prefix' => '',
                 'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             ]
