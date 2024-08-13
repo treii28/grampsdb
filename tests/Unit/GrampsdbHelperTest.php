dd7e55bc2586296186cf2a259110e4acd19a4d0b
Scott Webster Wood <treii28@gmail.com>
2024-08-13 12:24:39 -0400

    added helper test

---
+++ tests/Unit/GrampsdbHelperTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Treii28\Grampsdb;
+
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class GrampsdbHelperTest extends TestCase
+{
+    public function testHelper()
+    {
+        $db = DB::connection('grampsdb');
+        $this->assertInstanceOf('Illuminate\Database\Connection', $db);
+        /** @var \stdClass $record */
+        $record = $db->table('person')
+            ->select(['*'])
+            ->where('given_name', 'Scott Webster')
+            ->first();
+        $this->assertIsObject($record);
+        $blob = $record->blob_data;
+        $data = GrampsdbHelper::unpickle($blob);
+        $this->assertIsArray($data);
+    }
+}
